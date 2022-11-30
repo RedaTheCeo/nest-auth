761821696f0baeca01a15306fa54abb4b9c4ac3a
RedaTheCeo <ridabenassila@gmail.com>
2022-11-30 09:50:52 +0100

    hashed password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UsersService } from "../users/users.service";
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class AuthService {
@@ -8,12 +9,13 @@ export class AuthService {
 
     async login(username: string, pass: string) {
         const user = await this.usersService.findByUsername(username);
-        if (user && user.password === pass) {
+        const passMatched = await bcrypt.compare(pass, user.password);
+        console.log(passMatched);
+        if (user && passMatched) {
             const payload = { username: user.username, sub: user.id };
             return { access_token: this.jwtService.sign(payload) };
         } else {
             throw new HttpException("password inccorect", HttpStatus.FORBIDDEN);
         }
     }
-
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DataSource, Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class UsersService {
@@ -12,7 +13,9 @@ export class UsersService {
   ) { }
 
   async create(user: User) {
-    return await this.usersRepository.save(user);
+    const { password } = user
+    const hash = await bcrypt.hash(password, 10);
+    return await this.usersRepository.save({...user, password:hash});
   }
 
   findAll(): Promise<User[]> {
